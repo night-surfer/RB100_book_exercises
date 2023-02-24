3198c0b438a0489b5ff2c5495db3c007dbf2ed13
DannyZ <27746699+night-surfer@users.noreply.github.com>
2023-02-23 18:17:30 -0800

    Update exercise7.rb

--- ch.1_basics/exercise7.rb
+++ ch.1_basics/exercise7.rb
@@ -1,6 +1,6 @@
 # What does the following error message tell you?
 # That there is an opening bracket somewhere in the program without a closing bracket following it.
-# And that it was expecting a hash command
+# And that it was expecting a hash command.
 
 SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
   from /usr/local/rvm/rubies/ruby-2.5.3/bin/irb:16:in `<main>'
